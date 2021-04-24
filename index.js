019006cdca63e1241a3f0a71da147a3003a18a92
Jacob Studer <jacob.studer.dev@gmail.com>
2021-04-24 16:11:25 -0600

    changes to index.js, prompt function redone, etc

--- index.js
+++ index.js
@@ -1,50 +1,163 @@
 //This is where the questions go 
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateHtml = require()
+const Manager = require('./lib/Manager');
+// const Intern = require('./lib/Intern');
+// const Engineer = require('./lib/Engineer');
+const generateHtml = require('./src/template')
+const teamMembers = [];
+// const path = require('path');
+
 
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: 'What is the Managers name?',
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: 'What is the Managers ID Number?',
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: 'What is the Managers Email?',
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNum',
-            message: 'What is the Managers Office Number?',
-        },
-    ])
+
+//where to put inquirer?
+
+    const generateManager = () => {
+        console.log('Welcome To Team Builder Pro. Please start with your Manager details.')
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: 'What is the Managers name?',
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: 'What is the Managers ID Number?',
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: 'What is the Managers Email?',
+            },
+            {
+                type: 'input',
+                name: 'managerOfficeNum',
+                message: 'What is the Managers Office Number?',
+            },
+        ]) .then(answers => {
+            const manager = new Manager(
+                answers.managerName,
+                answers.managerId,
+                answers.managerEmail,
+                answers.managerOfficeNum,
+            )
+            teamMembers.push(manager); 
+            //function to do something with teamMembers array?
+        })
+
+        // pickEmployee();
+
+    }
+
+    const pickEmployee = () => {
+
+        // if (teamMembers.length === 5){
+        //     return console.log('Max amount of team members added.')
+        // };
+
+        //how to add a yes or no choice before this asking if they want to add another employee
+
+        prompt ([
+            {
+                type: 'input',
+                name: 'employeeChoice',
+                message: 'What type of Employee would you like to add next?',
+            }
+        ]) .then(answers => {
+            if (answers.employeeChoice === 'Intern') {
+                generateIntern();
+            } else if (answers.employeeChoice === 'Engineer'){
+                generateEngineer();
+            } else {
+                console.log('Please enter a valid Employee type.')
+            }
+        })
+    }
+
+    const generateIntern = () => {
+        prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: 'What is the Interns name?',
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: 'What is the Interns ID Number?',
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: 'What is the Interns Email?',
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: 'What School did the Intern go to?',
+            },
+        ]) .then(answers => {
+            const intern = new Intern(
+                answers.internName,
+                answers.internId,
+                answers.internEmail,
+                answers.internSchool,
+            )
+            teamMembers.push(intern);   
+        })
+    }
+
+    const generateEngineer = () => {
+        prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: 'What is the Engineers name?',
+            },
+            {
+                type: 'input',
+                name: 'engineerId',
+                message: 'What is the Engineers ID Number?',
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'What is the Engineers Email?',
+            },
+            {
+                type: 'input',
+                name: 'engineerGithub',
+                message: 'What is the Engineers Github account?',
+            },
+        ]) .then(answers => {
+            const engineer = new Engineer(
+                answers.engineerName,
+                answers.engineerId,
+                answers.engineerEmail,
+                answers.engineerGithub,
+            )
+            teamMembers.push(engineer);
+        })
+    }
+
+    generateManager();
+
 }
 
-const init = () => {
-    promptUser()
-    
-    .then((response) => {
-        writeToFile(response);
-    });  
-    };
-
-const writeToFile = (response) => {
+const writeToFile = () => {
+    console.log("Generating Team...")
     fs.writeFile('index.html', generateHtml(response), (err) =>
         err ? console.error(err) : console.log('Success!'))
 }
     
     
-init();
+promptUser();
 
-module.exports = promptUser;
+// module.exports = promptUser;
+//can this not export because its a function?
 
 
 //look into validation
--- src/template.js
+++ src/template.js
@@ -1,5 +1,3 @@
-
-
 const generateHtml = () => {
     return `<html lang="en">
     <head>
@@ -12,12 +10,19 @@ const generateHtml = () => {
     
         <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap" rel="stylesheet">
     
-        <title>Document</title>
+        <title>Team Builder Pro</title>
     </head>
     
     <body>
+
+    <h1>Team Builder Pro</h1>
+    <h2>The Command Line Team Builder</h2>
     
         <div>${generateManager}</div>
+
+        <div>${generateIntern}</div>
+        
+        <div>${generateEngineer}</div>
     
     
 
@@ -34,16 +39,47 @@ const generateHtml = () => {
     </html>`
 }
 
+//import input answers 
 const generateManager = () => {
     return `<div class="card" style="width: 18rem;">
     <div class="card-header">
-        <h5 class="card-title">${manager.getName()}</h5>
+        <h5 class="card-title">${manager.managerName}</h5>
         <h6 class="card-subtitle mb-2">${manager.getRole()}</h6>
     <div>
     <div>
-        <p class="card-text">ID: ${manager.getId()}</p>
-        <p>Email: ${manager.getEmail()}</p><a href="#" class="card-link"></a>
-        <p>OfficerNumber: ${manager.get}</p>
+        <p class="card-text">ID: ${manager.managerId}</p>
+        <p>Email: ${manager.managerEmail}</p><a href="#" class="card-link"></a>
+        <p>OfficerNumber: ${manager.officeNumber}</p>
+    </div>
+</div>
+`
+};
+
+const generateIntern = () => {
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <h5 class="card-title">${intern.internName}</h5>
+        <h6 class="card-subtitle mb-2">${intern.getRole()}</h6>
+    <div>
+    <div>
+        <p class="card-text">ID: ${intern.internId}</p>
+        <p>Email: ${intern.internEmail}</p><a href="#" class="card-link"></a>
+        <p>School: ${intern.internSchool}</p>
+    </div>
+</div>
+`
+};
+
+const generateEngineer = () => {
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <h5 class="card-title">${engineer.engineerName}</h5>
+        <h6 class="card-subtitle mb-2">${engineer.getRole()}</h6>
+    <div>
+    <div>
+        <p class="card-text">ID: ${engineer.internId}</p>
+        <p>Email: ${engineer.engineerEmail}</p><a href="#" class="card-link"></a>
+        <p>Github: ${engineer.engineerGitHub}</p>
     </div>
 </div>
 `
